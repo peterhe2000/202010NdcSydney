e6484f858c86f414c9e5714a48bea40c96e8eaca
Peter He <peter@netwealth.com.au>
2020-10-13 10:33:01 +1100

    Unit test for commandValidators

---
+++ CaWorkshop/CaWorkshop.Application.UnitTests/TodoLists/Commands/CreateTodoList/CreateTodoListCommandValidatorTests.cs
@@ -0,0 +1,42 @@
+﻿using CaWorkshop.Application.TodoLists.Commands.CreateTodoList;
+using CaWorkshop.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore.Internal;
+using Shouldly;
+using Xunit;
+
+namespace CaWorkshop.Application.UnitTests.TodoLists.Commands.CreateTodoList
+{
+    public class CreateTodoListCommandValidatorTests : TestFixture
+    {
+        [Fact]
+        public void IsValid_ShouldBeTrue_WhenListTitleIsUnique()
+        {
+            var command = new CreateTodoListCommand
+            {
+                Title = "Bucket List"
+            };
+
+            var validator = new CreateTodoListCommandValidator(Context);
+
+            var result = validator.Validate(command);
+
+            result.IsValid.ShouldBe(true);
+        }
+
+        [Fact]
+        public void IsValid_ShouldBeFalse_WhenListTitleIsNotUnique()
+        {
+            var command = new CreateTodoListCommand
+            {
+                Title = "Todo List"
+            };
+
+            var validator = new CreateTodoListCommandValidator(Context);
+
+            var result = validator.Validate(command);
+
+            // NOTE: An assumpiton is made that this has failed. because the title is not unique.
+            result.IsValid.ShouldBe(false);
+        }
+    }
+}
